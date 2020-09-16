0f531a7d89164324f776dec5aa6aa4437df290f8
kirill <031kirill2a@mail.ru>
2020-09-16 17:38:07 +0300

    полные именa

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int sumNum (int num) {
+    private static int sumNumber (int number) {
         int sum = 0;
         for(int i = 1; i < 4; i++) {
-            sum += num % 10;
-            num /= 10;
+            sum += number % 10;
+            number /= 10;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
-        int num = scan.nextInt();
-        System.out.printf("Sum of the last 3 digits: %d", sumNum(num));
+        int number = scanner.nextInt();
+        System.out.printf("Sum of the last 3 digits: %d", sumNumber(number));
     }
 }
