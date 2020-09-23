53967f99c31a1a2ab6217ee07dd8aadab6cf83bd
kirill <031kirill2a@mail.ru>
2020-09-23 13:41:07 +0300

    полные именa

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -2,18 +2,22 @@ package ru.vsu.cs.ivanov;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
-    private static int sumNumber (int number) {
+    private static int sumNumber (int number)
+    {
         int sum = 0;
-        for(int i = 1; i < 4; i++) {
+        for(int i = 1; i < 4; i++)
+        {
             sum += number % 10;
             number /= 10;
         }
         return sum;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
         int number = scanner.nextInt();
