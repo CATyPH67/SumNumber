5111ab57a8edcc97509445948d201955934209b2
kirill <031kirill2a@mail.ru>
2020-09-29 18:52:46 +0300

    3 метода

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -7,8 +7,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        int number = inputNumber();
-        conclusionSumNumber(number);
+        conclusionSumNumber(inputNumber());
     }
 
     private static int inputNumber  ()
