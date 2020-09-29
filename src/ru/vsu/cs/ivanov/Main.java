14b08d282e5eed5d189e2726ef892c6d281603aa
kirill <031kirill2a@mail.ru>
2020-09-29 19:03:12 +0300

    3 метода

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -5,28 +5,23 @@ import java.util.Scanner;
 public class Main
 {
 
-    public static void main(String[] args)
-    {
+    public static void main (String[] args) {
         conclusionSumNumber(inputNumber());
     }
 
-    private static int inputNumber  ()
-    {
+    private static int inputNumber () {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
         return scanner.nextInt();
     }
 
-    private static void conclusionSumNumber(int number)
-    {
+    private static void conclusionSumNumber (int number) {
         System.out.printf("Sum of the last 3 digits: %d", sumNumber(number));
     }
 
-    private static int sumNumber (int number)
-    {
+    private static int sumNumber (int number) {
         int sum = 0;
-        for(int i = 0; i < 3; i++)
-        {
+        for(int i = 0; i < 3; i++) {
             sum += number % 10;
             number /= 10;
         }
